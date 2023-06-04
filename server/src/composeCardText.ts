6463ef17039dc255ac715e23647c479d9174e30d
Hessamoddin A Shokravi <ahs502@gmail.com>
2023-06-04 17:52:58 +0330

    Fix no timezone in card text

--- server/src/composeCardText.ts
+++ server/src/composeCardText.ts
@@ -4,17 +4,17 @@ export function composeCardText(cardExtensionData: CardExtensionData): string | 
   let text: string | null = null
 
   if (cardExtensionData?.accepted) {
-    const from = new Date(cardExtensionData.fromTimestamp)
-    const fromDate = from.toDateString()
-    const fromTime = from.toTimeString().slice(0, 5)
-    const to = new Date(cardExtensionData.toTimestamp)
-    const toDate = to.toDateString()
-    const toTime = to.toTimeString().slice(0, 5)
+    const from = new Date(cardExtensionData.fromTimestamp).toUTCString() // e.g.: 'Sun, 04 Jun 2023 14:14:08 GMT'
+    const fromDate = from.slice(0, 16)
+    const fromTime = from.slice(17, 22)
+    const to = new Date(cardExtensionData.toTimestamp).toUTCString() // e.g.: 'Sun, 04 Jun 2023 14:14:08 GMT'
+    const toDate = to.slice(0, 16)
+    const toTime = to.slice(17, 22)
 
     text =
       fromDate === toDate
-        ? `On ${fromDate}, from ${fromTime} to ${toTime}`
-        : `From ${fromDate} ${fromTime} to ${toDate} ${toTime}`
+        ? `On ${fromDate}, from ${fromTime} to ${toTime} UTC`
+        : `From ${fromDate} ${fromTime} to ${toDate} ${toTime} UTC`
 
     if (cardExtensionData.reservedBy) {
       text += `\nResereved by ${cardExtensionData.reservedBy.name} (${cardExtensionData.reservedBy.email})`
